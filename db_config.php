64f9dc1b40c1b33a53d9d6966451ec0bd97b4551
Amr Khaled AL-Awadhi <amrawadi@icloud.com>
2025-01-13 00:01:37 +0300

    Add files via upload

---
+++ db_config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_data";
+
+// إنشاء الاتصال
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// التحقق من الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
+?>
